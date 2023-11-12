103bc584d4ef4f4947d5761d142b148fa269b0c0
Tlotliso <tlotlisomokati@gmail.com>
2023-11-12 10:48:50 +0200

    Added Map and locations

--- StoreLocator/resources/views/home.blade.php
+++ StoreLocator/resources/views/home.blade.php
@@ -1,16 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <x-maps-leaflet></x-maps-leaflet>
+    <div id="map" class="container" style="width: 80%; height:70vh; zIndex:0;"></div>
 
-        {{-- set the centerpoint of the map: --}}
-        <x-maps-leaflet :centerPoint="['lat' => 52.16, 'long' => 5]"></x-maps-leaflet>
+    <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    <script src="https://unpkg.com/leaflet-providers@1.12.0/leaflet-providers.js"></script>
 
-        {{-- set a zoomlevel: --}}
-        <x-maps-leaflet :zoomLevel="6"></x-maps-leaflet>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var maseruLatitude = -29.313443081134025;
+            var maseruLongitude = 27.48445138879433;
+            var map = L.map('map').setView([maseruLatitude, maseruLongitude], 13);
 
-        {{-- Set markers on the map: --}}
-        <x-maps-leaflet :markers="[['lat' => 52.16444513293423, 'long' => 5.985622388024091]]"></x-maps-leaflet>
-    </div>
+            L.tileLayer.provider('OpenStreetMap.Mapnik').addTo(map);
+
+            var stores = @json($stores);
+
+            stores.forEach(function(store) {
+                var marker = L.marker([parseFloat(store.longitude), parseFloat(store.latitude)]).addTo(map);
+                marker.bindPopup('<b>' + store.storeName + '</b><br>' + store.storeAddress);
+            });
+        });
+    </script>
 @endsection
--- StoreLocator/resources/views/layouts/app.blade.php
+++ StoreLocator/resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
